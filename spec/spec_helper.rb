2364847099dc7cd938f0e0f18f1539fb37402a49
Yossef Mendelssohn <ymendel@pobox.com>
2008-04-20 19:56:52 -0500

    Initial repo commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib/')
+
+require 'rubygems'
+require 'mocha'
+
+Spec::Runner.configure do |config|
+  config.mock_with :mocha
+end
+
+def setup_rails_database
+  dir = File.dirname(__FILE__)
+  
+  ENV['RAILS_ENV'] ||= 'test'
+  require "#{dir}/../../../../config/environment"
+
+  require "#{dir}/../init.rb"
+end
