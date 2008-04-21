8bba38e14b3742967bf381c5c144ea505d60e5c3
Yossef Mendelssohn <ymendel@pobox.com>
2008-04-20 20:12:58 -0500

    Added specs for forwarding a message down a few levels of chained method call.

--- spec/shmemeter_spec.rb
+++ spec/shmemeter_spec.rb
@@ -36,4 +36,40 @@ describe 'delegation' do
       @object.forwarded_method.should == val
     end
   end
+  
+  describe 'of a message to an arbitrarily-deeply-nested object' do
+    before :each do
+      @class = Struct.new(:down) do
+        delegate :forwarded_method, :to => 'down.deep.target'
+      end
+      
+      @target = stub('target')
+      deep = Struct.new(:target).new(@target)
+      down = Struct.new(:deep).new(deep)
+      @object = @class.new(down)
+    end
+    
+    it 'should forward the message' do
+      @target.expects(:forwarded_method)
+      @object.forwarded_method
+    end
+    
+    it 'should pass along an argument' do
+      arg = stub('arg')
+      @target.expects(:forwarded_method).with(arg)
+      @object.forwarded_method(arg)
+    end
+    
+    it 'should pass along multiple arguments' do
+      args = Array.new(3) { stub('arg') }
+      @target.expects(:forwarded_method).with(*args)
+      @object.forwarded_method(*args)
+    end
+    
+    it 'should return the value from the object' do
+      val = stub('val')
+      @target.stubs(:forwarded_method).returns(val)
+      @object.forwarded_method.should == val
+    end
+  end
 end
